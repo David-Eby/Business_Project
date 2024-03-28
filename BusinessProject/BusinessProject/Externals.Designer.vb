289a943ccdfc39a13da4df8fb48ca43a1d3352e3
David Eby <dqeby606@learner.dvusd.org>
2024-03-28 08:02:37 -0700

    No Color

--- BusinessProject/BusinessProject/Externals.Designer.vb
+++ BusinessProject/BusinessProject/Externals.Designer.vb
@@ -1,4 +1,4 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class Externals
     Inherits System.Windows.Forms.Form
 
